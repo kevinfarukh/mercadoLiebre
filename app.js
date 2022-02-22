e317eb7780a0a0a220447ec9df23522e9dfa292b
kevinfarukh <kevinfarukh@gmail.com>
2022-02-22 18:51:42 -0500

    archivos mercado liebre

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+const path = require("path")
+
+app.use(express.static(path.resolve(__dirname,"./public")))
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve("./views/home.html"))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.resolve("./views/register.html"))
+})
+app.post("/",(req,res)=>{
+    res.sendFile(path.resolve("./views/home.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve("./views/login.html"))
+})
+app.post("/",(req,res)=>{
+    res.sendFile(path.resolve("./views/home.html"))
+})
+app.listen(3010, ()=>{
+    console.log("servidor corriendo en el puerto 3010") 
+})
